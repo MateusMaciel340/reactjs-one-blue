60b652d05b4cad614946ff89d4e5e2f8854bd763
MateusMaciel340 <mateusmaciel340@gmail.com>
2022-05-24 20:16:38 -0300

    Criação e aplicação da validação (Yup | Formik)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,59 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-function Form(){
+import * as yup from "yup";
+import { useFormik } from "formik";
+
+import * as Style from "../styles/style";
+
+function Form(props){
+
+    const formik = useFormik({
+        initialValues:{
+            name: "", password: ""
+        },
+        validationSchema: yup.object({
+            name: yup.string().required("O campo usuário é obrigatório!"),
+            password: yup.string().required("O campo senha é obrigatório!")
+        }),
+        onSubmit: (values) =>{
+            alert(JSON.stringify(values, null, 2));
+        },
+    })
+
     return(
-        <>
+        <form onSubmit={formik.handleSubmit}>
             <TextField
-                margin="normal" required fullWidth id="email"
-                label="Digite seu email" name="email" autoComplete="email"
-                autoFocus
+                margin="normal" fullWidth id="name" type="text"
+                name="name" autoComplete="name"
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur} value={formik.values.name}
+
+                label={"Digite seu nome de usuário"}
             />
 
+            {formik.touched.name && formik.errors.name ? (
+                <Style.MessageLogin>{formik.errors.name}</Style.MessageLogin>
+            ): null}
+
             <TextField
-                margin="normal" required fullWidth name="password"
-                label="Digite sua senha" type="password" id="password"
-                autoComplete="current-password"
+                margin="normal" fullWidth name="password"
+                type="password" id="password"
+                autoComplete="current-password" onChange={formik.handleChange}
+                onBlur={formik.handleBlur} value={formik.values.password}
+
+                label={"Digite sua senha"}
             />
+
+            {formik.touched.password && formik.errors.password ? (
+                <Style.MessageLogin>{formik.errors.password}</Style.MessageLogin>
+            ): null}
 
             <Button type="submit" fullWidth
                 variant="contained" sx={{ mt: 3, mb: 2 }}>
                     Entrar
             </Button>
-        </>
+        </form>
     )
 }
 
--- src/components/registration/FormLabel.jsx
+++ src/components/registration/FormLabel.jsx
@@ -2,23 +2,61 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 
+import * as yup from "yup";
+import { useFormik } from "formik";
+
+import * as Style from "../../styles/style";
+
 function FormLabel(){
+
+    const formik = useFormik({
+        initialValues:{
+            name: "", password: "", password_repeat: ""
+        },
+        validationSchema: yup.object({
+            name: yup.string().required("O campo usuário é obrigatório!"),
+            password: yup.string().required("O campo senha é obrigatório!"),
+            password_repeat: yup.string().required("Repita sua senha anterior!")
+        }),
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return(
-        <>
+        <form onSubmit={formik.handleSubmit}>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={12}>
-                    <TextField autoComplete="given-name" name="email" required
-                        fullWidth id="email" label="Digite um email" autoFocus/>
+                    <TextField autoComplete="given-name" name="name"
+                        fullWidth id="name" label="Digite o nome de usuário"
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                        value={formik.values.name}/>
+
+                    {formik.touched.name && formik.errors.name ? (
+                        <Style.Message>{formik.errors.name}</Style.Message>
+                    ) : null}
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                    <TextField autoComplete="given-name" name="email" required
-                        fullWidth id="email" label="Digite uma senha" autoFocus/>
+                    <TextField autoComplete="given-password" name="password"
+                        fullWidth id="password" label="Digite uma senha"
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                        value={formik.values.password}/>
+
+                    {formik.touched.password && formik.errors.password ? (
+                        <Style.Message>{formik.errors.password}</Style.Message>
+                    ) : null}
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                    <TextField autoComplete="given-name" name="email-repeat" required
-                        fullWidth id="email-repeat" label="Repita sua senha" autoFocus/>
+                    <TextField autoComplete="given-password-repeat" name="password_repeat"
+                        fullWidth id="password_repeat" label="Repita sua senha"
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                        value={formik.values.password_repeat}/>
+
+                    {formik.touched.password_repeat && formik.errors.password_repeat ? (
+                        <Style.Message>{formik.errors.password_repeat}</Style.Message>
+                    ) : null}
                 </Grid>
             </Grid>
 
@@ -27,7 +65,7 @@ function FormLabel(){
                 variant="contained" sx={{ mt: 3, mb: 2 }}>
                     Cadastrar
             </Button>
-        </>
+        </form>
     )
 }
 
